bc66de3b0c47acaac98a0f89d77f92547ae20d3d
pedrobzz <pedrobzz66@gmail.com>
2023-03-17 15:08:58 -0300

    feat(post schema): validate cuid

--- packages/api/src/router/post/schema.ts
+++ packages/api/src/router/post/schema.ts
@@ -1,13 +1,13 @@
 import { z } from "zod";
 
-export const getPostByIdSchema = z.object({ id: z.string() });
+export const getPostByIdSchema = z.object({ id: z.string().cuid() });
 
 export const createPostSchema = z.object({
   title: z.string().min(1),
   content: z.string().min(1),
 });
 
-export const deletePostSchema = z.object({ id: z.string() });
+export const deletePostSchema = z.object({ id: z.string().cuid() });
 
 export type CreatePostSchema = z.infer<typeof createPostSchema>;
 export type DeletePostSchema = z.infer<typeof deletePostSchema>;
