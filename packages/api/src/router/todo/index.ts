25d3752b69d0d1af6f6273150f8ad52f43022848
pedrobzz <pedrobzz66@gmail.com>
2023-03-17 21:53:04 -0300

    feat(todos): enforce status by zod, as we can't use enums at prisma level (sqlite doesn't support)

--- packages/api/src/router/todo/index.ts
+++ packages/api/src/router/todo/index.ts
@@ -1,12 +1,15 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../../trpc";
-import { createTodoSchema, getTodoByIdSchema } from "./schema";
+import { createTodoSchema, getTodoByIdSchema, todoSchema } from "./schema";
 import { createTodo } from "./service";
 
 export const todoRouter = createTRPCRouter({
-  all: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.todo.findMany({ orderBy: { id: "desc" } });
+  all: publicProcedure.query(async ({ ctx }) => {
+    const prismaTodos = await ctx.prisma.todo.findMany({
+      orderBy: { updatedAt: "desc" },
+    });
+    return prismaTodos.map((todo) => todoSchema.parse(todo));
   }),
   byId: publicProcedure.input(getTodoByIdSchema).query(({ ctx, input }) => {
     return ctx.prisma.todo.findFirst({ where: { id: input.id } });
--- packages/api/src/router/todo/schema.ts
+++ packages/api/src/router/todo/schema.ts
@@ -1,10 +1,24 @@
 import { z } from "zod";
 
+const possibleStatus = ["todo", "doing", "done"] as const;
+
+export const todoSchema = z.object({
+  id: z.string().cuid(),
+  title: z.string().min(1),
+  content: z.string().min(1),
+
+  status: z.enum(possibleStatus),
+
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
 export const getTodoByIdSchema = z.object({ id: z.string().cuid() });
 
 export const createTodoSchema = z.object({
   title: z.string().min(1),
   content: z.string().min(1),
+  status: z.enum(possibleStatus).default("todo"),
 });
 
 export const deleteTodoSchema = z.object({ id: z.string().cuid() });
@@ -13,6 +27,7 @@ export const updateTodoSchema = z.object({
   id: z.string().cuid(),
   title: z.string().min(1).optional(),
   content: z.string().min(1).optional(),
+  status: z.enum(possibleStatus).optional(),
 });
 
 export type CreateTodoSchema = z.infer<typeof createTodoSchema>;
